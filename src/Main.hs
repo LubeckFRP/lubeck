47115fed2ddb2923c120e0b37a4b941c6f3bea73
Hans <hans@hanshoglund.se>
2015-12-14 15:17:13 +0000

    Build with stack

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 
 
 -- import Music.Pitch
-import Control.Monad.Plus
+import Control.Monad.Plus (partial, predicate)
 
 -- main = print $ (cs :: Pitch) .+^ (_P8^*(-3) ^+^ m3)
-main = print "Hello"
+main = do
+  print "Hello Tom!"
+  print (partial odd $ 11245521)
