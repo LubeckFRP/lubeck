9544fba92c5a8544be6ec67aa5f71119b8f9bb91
Hans <hans@hanshoglund.se>
2015-12-15 12:15:31 +0000

    Serve results with servant

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -1,2 +1,12 @@
 
+
+import Servant
+import Servant.Utils.StaticFiles (serveDirectory)
+
+type GhcJsTestServer = Raw
+
+server :: Server GhcJsTestServer
+-- TODO how to get this from Stac
+server = serveDirectory ".stack-work/install/x86_64-osx/nightly-2015-12-14/ghcjs-0.2.0_ghc-7.10.2/bin/ghcjs-test.jsexe"
+
 main = print "Server!"
