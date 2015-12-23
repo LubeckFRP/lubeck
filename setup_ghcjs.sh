943e6cb1e3eb50b4eb05fc5f5ca9b303ebdf8526
Tom Nielsen <tanielsen@gmail.com>
2015-12-23 12:05:40 +0000

    working vagrant setup

---
+++ setup_ghcjs.sh
@@ -0,0 +1,25 @@
+export PATH=/home/vagrant/.cabal/bin:/home/vagrant/.local/bin:/opt/happy/1.19.5/bin:/opt/alex/3.1.4/bin:/opt/cabal/1.22/bin:/opt/ghc/7.10.2/bin:$PATH
+
+cd ~
+
+git clone https://github.com/ghcjs/ghcjs.git || true
+
+cd ghcjs
+
+stack setup && stack install -j2
+
+ghcjs-boot --dev
+
+cd ~/ghcjs-test
+
+cabal update
+
+cabal sandbox delete || true
+
+cabal sandbox init
+
+cabal sandbox add-source ../ghcjs-vdom
+
+cabal sandbox add-source ../ghcjs-ffiqq
+
+cabal install --ghcjs
