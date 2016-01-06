7a3506f37dd52da4c9d86c046ea08aabab338678
Hans <hans@hanshoglund.se>
2016-01-06 16:00:31 +0000

    Temporary index page

--- src/Index.hs
+++ src/Index.hs
@@ -8,13 +8,19 @@ import qualified Prelude
 
 import GHCJS.Types(JSString, jsval)
 import GHCJS.VDOM.Event (click, change, submit, stopPropagation, preventDefault, value)
-import GHCJS.VDOM.Element (p, h1, div, text, form, button, img, hr, custom)
-import GHCJS.VDOM.Attribute (src, width, class_)
+import GHCJS.VDOM.Element (a, p, h1, div, text, form, button, img, hr, ul, li, custom)
+import GHCJS.VDOM.Attribute (src, href, width, class_)
 
 import Lubeck.App (Html, runAppStatic)
 
 page :: Html
-page = h1 () [text "Index"]
+page = div () [h1 () [text "Index"]
+  , ul ()
+    [ li () [a (href "adplatform/")   [text "Ad platform"]]
+    , li () [a (href "interactions/") [text "Interaction browser"]]
+    , li () [a (href "posts/")        [text "Post search"]]
+    ]
+  ]
 
 -- MAIN
 
