93eb8b058d39bac2615595e7c87c474075b4ab06
Hans <hans@hanshoglund.se>
2016-01-06 18:07:31 +0000

    Post search queries

--- src/BD/Query/PostQuery.hs
+++ src/BD/Query/PostQuery.hs
@@ -1,11 +1,16 @@
 
+{-# LANGUAGE NamedFieldPuns #-}
+
 module BD.Query.PostQuery (
   PostQuery(..),
-  SimplePostQuery
+  SimplePostQuery,
+  defSimplePostQuery,
+  complexifyPostQuery
 ) where
 
-import Data.Time.Calendar (Day)
-import Numeric.Interval (Interval)
+import Data.Time.Calendar (Day(..))
+import Numeric.Interval (Interval, whole, (...))
+import qualified Numeric.Interval as I
 
 data PostQuery
   = PostQueryInCaption String
@@ -34,8 +39,8 @@ data SimplePostQuery = SimplePostQuery {
     hashTag      :: String,
     userName     :: String,
 
-    followers    :: Interval Int,
-    date         :: Interval Day,
+    followers    :: Interval (Maybe Int), -- Nothing for inf
+    date         :: Interval (Maybe Day),
     location     :: Maybe Int,
 
     orderBy     :: PostOrder,
@@ -54,3 +59,41 @@ data SortDirection
   = Asc
   | Desc
   deriving (Eq, Ord, Show)
+
+defSimplePostQuery :: SimplePostQuery
+defSimplePostQuery = SimplePostQuery {
+    caption   = "",
+    comment   = "",
+    hashTag   = "",
+    userName  = "",
+    followers = Nothing ... Nothing,
+    date      = Nothing ... Nothing,
+    location  = Nothing,
+    orderBy   = PostByLikes,
+    direction = Asc
+  }
+
+complexifyPostQuery :: SimplePostQuery -> PostQuery
+complexifyPostQuery (SimplePostQuery {caption, comment, hashTag, userName, followers, date, location, orderBy, direction}) =
+    PostQueryAnd $
+       (if caption  == "" then [] else [PostQueryInCaption caption])
+    ++ (if comment  == "" then [] else [PostQueryHasComment comment])
+    ++ (if hashTag  == "" then [] else [PostQueryHashtag hashTag])
+    ++ (if userName == "" then [] else [PostQueryUsername userName])
+    ++ complexifyInterval 0       PostQueryFollowers followers
+    ++ complexifyInterval someDay PostQueryDate date
+    ++ [PostQueryOrderBy orderBy, PostQueryOrderDirection direction]
+  where
+    someDay = ModifiedJulianDay 0
+    complexifyInterval z f x = fmap (uncurry f) (intervalToOrderings z x)
+
+-- | Convert an interval to a list of restrictions.
+-- First argument is an arbitrary value of the type.
+intervalToOrderings :: a -> Interval (Maybe a) -> [(Ordering, a)]
+intervalToOrderings arbitrary i
+  | I.null i  = [(GT,arbitrary),(LT,arbitrary)] -- Not quite, should be orderings that denote to the empty set such as for any x [(GT,x),(LT,x)]
+  | otherwise = case (I.inf i, I.sup i) of
+    (Nothing, Nothing) -> []
+    (Nothing, Just b)  -> [(LT, b)]
+    (Just a,  Nothing) -> [(GT, a)]
+    (Just a,  Just b)  -> [(GT, a), (LT, b)]
