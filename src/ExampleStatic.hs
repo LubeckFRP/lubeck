365231cb060c2d637ecadddb8c444fdd334cf6f1
Hans <hans@hanshoglund.se>
2016-01-26 17:49:44 +0000

    Fix bug in static example

--- src/ExampleStatic.hs
+++ src/ExampleStatic.hs
@@ -7,7 +7,7 @@ import Prelude hiding (div)
 import qualified Prelude
 
 import GHCJS.Types(JSString, jsval)
-import Web.VirtualDom.Html (p, h1, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label, li, ul)
+import Web.VirtualDom.Html (p, h1, h2, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label, li, ul)
 import Web.VirtualDom.Html.Events (click, change, keyup, submit, stopPropagation, preventDefault, value)
 import Web.VirtualDom.Html.Attributes (src, width, class_, href, target, width, src)
 
@@ -15,14 +15,15 @@ import Lubeck.App (Html, runAppStatic)
 
 page :: Html
 page = div [] [h1 [] [text "Index"]
-  , h1 [] [text "Smaller index"]
+  , h2 [] [text "Smaller index"]
   , ul []
     [ li [] [text "First thing"]
     , li [] [text "Second thing"]
     ]
   ]
 
+
 -- MAIN
 
 main :: IO ()
-main = runAppStatic page
+main = runAppReactive (pure page)
