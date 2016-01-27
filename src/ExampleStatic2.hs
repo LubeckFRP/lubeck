39e707917361c10c88e3b5dee660b90da2135158
tdietert <tdietert@mail.usf.edu>
2016-01-27 18:02:59 +0000

    Added static example for thomasd

---
+++ src/ExampleStatic2.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings, QuasiQuotes, TemplateHaskell, OverloadedStrings, TupleSections #-}
+
+module Main where
+
+import Prelude hiding (div)
+import qualified Prelude
+
+import GHCJS.Types (JSString, jsval)
+import qualified Web.VirtualDom as VD
+import qualified Web.VirtualDom.Html as VDHtml
+import qualified Web.VirtualDom.Html.Events as Event
+import qualified Web.VirtualDom.Html.Attributes as Attr
+
+import Lubeck.App (Html, runAppStatic)
+
+color :: JSString -> VD.Property
+color = VD.property "color" . jsval
+
+page :: Html
+page = VDHtml.div [color "red"] [VDHtml.h1 [] [VDHtml.text "Heading1!"]
+  , VDHtml.h3 [] [VDHtml.text "Heading3!!!"]
+  , VDHtml.ol [] 
+    [ VDHtml.li [color "blue"] [VDHtml.text "Item1?"]
+    , VDHtml.li [color "green"] [VDHtml.text "Item2??"]
+    ]
+  ]
+
+main :: IO ()
+main = runAppStatic page
