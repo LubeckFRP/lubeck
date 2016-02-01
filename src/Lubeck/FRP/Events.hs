38af000f2208ffd4510414a9ba079fa69b4fb80f
Hans <hans@hanshoglund.se>
2016-02-01 17:59:01 +0000

    Clean up FRP.Events, use better names in API

--- src/Lubeck/FRP/Events.hs
+++ src/Lubeck/FRP/Events.hs
@@ -1,8 +1,7 @@
 
 module Lubeck.FRP.Events
-    ( overs
-    , lookups
-    , oversG
+    ( updatesAtIndex
+    , updatesAtKey
     ) where
 
 import Lubeck.FRP
@@ -17,19 +16,20 @@ import Data.Map (Map)
 -- the original event has an occurance with at least one element,
 -- the second when the original has at least two elements, and so on.
 --
-overs :: Events [a] -> [Events (Maybe a)]
-overs e = fmap (\n -> fmap (`safeIndex` n) e) [0..]
+updatesAtIndex :: Events [a] -> [Events (Maybe a)]
+updatesAtIndex e = fmap (updatesAt (flip safeIndex) e) [0..]
   where
     safeIndex :: [a] -> Int -> Maybe a
     safeIndex xs n = if n < 0
         then Nothing
         else Data.Maybe.listToMaybe (drop n xs)
 
+-- |
+-- Propagate events on a particular key.
+--
+updatesAtKey :: Ord k => Events (Map k a) -> k -> Events (Maybe a)
+updatesAtKey = updatesAt Data.Map.lookup
 
-lookups :: Ord k => Events (Map k a) -> k -> Events (Maybe a)
-lookups = oversG Data.Map.lookup
-
--- oversG :: (k -> Map k b -> Maybe b) -> Events (Map k b) -> k -> Events (Maybe b)
-
-oversG :: (a -> b -> c) -> Events b -> a -> Events c
-oversG f e n = fmap (f n) e
+-- More general version
+updatesAt :: (a -> b -> c) -> Events b -> a -> Events c
+updatesAt f e n = fmap (f n) e
