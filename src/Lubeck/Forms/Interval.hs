271d176ed4aca0f8b4e2641afc64132b22765e72
Hans <hans@hanshoglund.se>
2016-02-02 13:56:48 +0000

    Cleanup Forms.Interval

--- src/Lubeck/Forms/Interval.hs
+++ src/Lubeck/Forms/Interval.hs
@@ -33,52 +33,9 @@ data WRange = Any | LessThen | GreaterThen | Between deriving (Show, Eq)
 
 integerIntervalWidget :: JSString -> Widget' (Interval (Maybe Int))
 integerIntervalWidget = customIntervalWidget 0 hideableIntegerWidget
---
+
 dateIntervalWidget :: Day -> JSString -> Widget' (Interval (Maybe Day))
 dateIntervalWidget dayNow = customIntervalWidget dayNow hideableDateWidget
-
-customIntervalWidget :: Ord a => a -> (Bool -> Widget' a) -> JSString -> Widget' (Interval (Maybe a))
-customIntervalWidget z numW title = id
-    $ mapHtmlWidget (\x -> E.div [A.class_ "form-group form-inline"] $ pure $ E.label [] [E.text title, x])
-    $ lmapWidget fromInterval
-    $ rmapWidget toInterval
-    $ spanWidget2
-  where
-    -- fromInterval :: Ord a => Interval (Maybe a) -> (String, (a, a))
-    -- toInterval   :: Ord a => (String, (a, a))   -> Interval (Maybe a)
-    fromInterval i
-      | I.null i = ((Any), (z,z))
-      | otherwise = case (I.inf i, I.sup i) of
-        (Just x,  Nothing) -> (GreaterThen, (x,x))
-        (Nothing, Just y)  -> (LessThen,    (y,y))
-        (Just x,  Just y)  -> (Between,     (x,y))
-        _                  -> (Any,         (z,z))
-    toInterval x = case x of
-      (Any,         (_,_)) -> Nothing I.... Nothing
-      (GreaterThen, (x,_)) -> Just x  I.... Nothing
-      (LessThen,    (_,y)) -> Nothing I.... Just y
-      (Between,     (x,y)) -> Just x  I.... Just y
-
-    -- spanWidget2 :: Widget' (JSString, (a, a))
-    -- spanTypeW :: Widget' JSString
-    -- numsW :: JSString -> Widget' (a, a)
-    spanWidget2 s x = composeWidget spanTypeW (numsW $ fst x) s x
-    spanTypeW = selectWidget
-      [ (Any,         "Any")
-      , (LessThen,    "Less than")
-      , (GreaterThen, "Greater than")
-      , (Between,     "Between")
-      ]
-    visible x = case x of
-      Any         -> (False, False)
-      GreaterThen -> (True, False)
-      LessThen    -> (False, True)
-      Between     -> (True, True)
-    numsW infFin = composeWidget (numW (fst $ visible infFin)) (numW (snd $ visible infFin))
-
--- TODO is the (Monoid Html) instance what we need?
-composeWidget :: Widget' a -> Widget' b -> Widget (a,b) (a,b)
-composeWidget a b = bothWidget mappend (subWidget Control.Lens._1 a) (subWidget Control.Lens._2 b)
 
 hideableIntegerWidget :: Bool -> Widget' Int
 hideableIntegerWidget False _ _ = mempty
@@ -114,3 +71,49 @@ hideableDateWidget True sink val = E.input
       Nothing -> return ()
 
     showDate = formatDateUTC
+
+
+customIntervalWidget :: Ord a => a -> (Bool -> Widget' a) -> JSString -> Widget' (Interval (Maybe a))
+customIntervalWidget z numW title = id
+    $ mapHtmlWidget (\x -> E.div [A.class_ "form-group form-inline"] $ pure $ E.label [] [E.text title, x])
+    $ lmapWidget fromInterval
+    $ rmapWidget toInterval
+    $ spanWidget2
+  where
+    -- fromInterval :: Ord a => Interval (Maybe a) -> (String, (a, a))
+    -- toInterval   :: Ord a => (String, (a, a))   -> Interval (Maybe a)
+    fromInterval i
+      | I.null i = ((Any), (z,z))
+      | otherwise = case (I.inf i, I.sup i) of
+        (Just x,  Nothing) -> (GreaterThen, (x,x))
+        (Nothing, Just y)  -> (LessThen,    (y,y))
+        (Just x,  Just y)  -> (Between,     (x,y))
+        _                  -> (Any,         (z,z))
+    toInterval x = case x of
+      (Any,         (_,_)) -> Nothing I.... Nothing
+      (GreaterThen, (x,_)) -> Just x  I.... Nothing
+      (LessThen,    (_,y)) -> Nothing I.... Just y
+      (Between,     (x,y)) -> Just x  I.... Just y
+
+    -- spanWidget2 :: Widget' (JSString, (a, a))
+    spanWidget2 s x = composeWidget spanTypeW (numsW $ fst x) s x
+
+    -- spanTypeW :: Widget' WRange
+    spanTypeW = selectWidget
+      [ (Any,         "Any")
+      , (LessThen,    "Less than")
+      , (GreaterThen, "Greater than")
+      , (Between,     "Between")
+      ]
+    visible x = case x of
+      Any         -> (False, False)
+      GreaterThen -> (True, False)
+      LessThen    -> (False, True)
+      Between     -> (True, True)
+
+    -- numsW :: JSString -> Widget' (a, a)
+    numsW infFin = composeWidget (numW (fst $ visible infFin)) (numW (snd $ visible infFin))
+
+-- TODO is the (Monoid Html) instance what we need?
+composeWidget :: Widget' a -> Widget' b -> Widget (a,b) (a,b)
+composeWidget a b = bothWidget mappend (subWidget Control.Lens._1 a) (subWidget Control.Lens._2 b)
