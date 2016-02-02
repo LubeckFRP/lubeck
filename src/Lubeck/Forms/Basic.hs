501f6dbd295f033e95e322b371853a2b5f8a001c
Hans <hans@hanshoglund.se>
2016-02-02 20:33:00 +0000

    Add some basic widgets

--- src/Lubeck/Forms/Basic.hs
+++ src/Lubeck/Forms/Basic.hs
@@ -2,7 +2,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lubeck.Forms.Basic
-    ( hideableDateWidget
+    ( rangeWidget
+    , integerWidget
+    , dateWidget
+    , hideableDateWidget
     , hideableIntegerWidget
     ) where
 
@@ -22,9 +25,28 @@ import Lubeck.Forms
 import Lubeck.Forms.Select
 import Lubeck.Util(formatDateUTC, parseDateUTC)
 
-hideableIntegerWidget :: Bool -> Widget' Int
-hideableIntegerWidget False _ _ = mempty
-hideableIntegerWidget True sink val = E.input
+rangeWidget :: Int -> Int -> Int -> Widget' Int
+rangeWidget minBound maxBound step
+  sink val = E.input
+  [ A.class_ "form-control"
+  , A.type_ "number"
+  , A.min minBound
+  , A.max maxBound
+  , A.step step
+  -- , A.style_ $ if enabled then "" else "visibility:hidden"
+  , Ev.change $ \e -> do
+      case Ev.value e of
+        "" -> do
+          print "Empty value for Int, ignoring."
+          return ()
+        s -> sink $ read $ unpack $ Ev.value e
+
+  , A.value (pack $ show val)
+  ]
+  []
+
+integerWidget :: Widget' Int
+integerWidget sink val = E.input
   [ A.class_ "form-control"
   , A.type_ "number"
   -- , A.style_ $ if enabled then "" else "visibility:hidden"
@@ -39,9 +61,8 @@ hideableIntegerWidget True sink val = E.input
   ]
   []
 
-hideableDateWidget :: Bool -> Widget' Day
-hideableDateWidget False _ _ = mempty
-hideableDateWidget True sink val = E.input
+dateWidget :: Widget' Day
+dateWidget sink val = E.input
   [ A.class_ "form-control"
   , A.type_ "date"
   , Ev.change $ \e -> maybeSink sink $ parseDateUTC $ unpack $ Ev.value e
@@ -54,5 +75,12 @@ hideableDateWidget True sink val = E.input
       -- <input>'s value can be set to ""
       -- we probably should not ignore this, but pass upstream and cancel given date boundary?
       Nothing -> return ()
-
     showDate = formatDateUTC
+
+hideableIntegerWidget :: Bool -> Widget' Int
+hideableIntegerWidget False = const $ const mempty
+hideableIntegerWidget True  = integerWidget
+
+hideableDateWidget :: Bool -> Widget' Day
+hideableDateWidget False = const $ const mempty
+hideableDateWidget True  = dateWidget
