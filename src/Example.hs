452eb7ab643f69e62d104d3c94b5ac64de3e989e
Hans <hans@hanshoglund.se>
2016-02-02 18:51:51 +0000

    Test drawing

--- src/Example.hs
+++ src/Example.hs
@@ -9,12 +9,9 @@ import qualified Prelude
 import Data.Monoid ((<>))
 
 import GHCJS.Types(JSString, jsval)
-import Web.VirtualDom.Html (p, h1, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label)
-import Web.VirtualDom.Html.Events (click, change, keyup, submit, stopPropagation, preventDefault, value)
-import Web.VirtualDom.Html.Attributes (src, width, class_, href, target, width, src)
-import qualified Web.VirtualDom.Html as E
-import qualified Web.VirtualDom.Html.Attributes as A
-import qualified Web.VirtualDom.Html.Events as Ev
+import qualified Web.VirtualDom.Html as H
+import qualified Web.VirtualDom.Html.Attributes as H
+import qualified Web.VirtualDom.Html.Events as H
 import qualified Web.VirtualDom.Svg.Events as SvgEv
 import qualified Data.JSString
 
@@ -24,29 +21,36 @@ import Lubeck.Forms (Widget, Widget', component)
 -- import Lubeck.Drawing (drawTest)
 import Lubeck.Drawing hiding (text)
 
+import Data.Colour (withOpacity)
 import qualified Data.Colour.Names as Colors
 
 
 render :: Widget' Int
-render output model = div []
-  [ div [] $ pure $ h1 [] $ pure $ text (Data.JSString.pack $ replicate model '!')
-  , div [] $ pure $ button
-    [ click (\_ -> output (succ model)) ]
-    [ text "Click me" ]
+render output model = H.div []
+  [ H.div [] $ pure $ H.h1 [] $ pure $ H.text (Data.JSString.pack $ replicate model '!')
+  , H.div [] $ pure $ H.button
+    [ H.click (\_ -> output (succ model)) ]
+    [ H.text "Click me" ]
   ,
-
   -- drawTest (length $ Data.JSString.unpack $ model)
   toSvg (RenderingOptions (Point 400 400) Center) $ drawing output model
 
   ]
 
-drawing output n = (addProperty (SvgEv.onClick $ \_ -> output (pred n)) (redCircle n)) <> xyAxis <> smokeBackground
-redCircle n = fillColor Colors.red $
-  translateY (5 * fromIntegral n) $ translateX (10 * fromIntegral n) $
-  scale (50 + 5 * fromIntegral n) $
-  circle
+drawing :: Sink Int -> Int -> Drawing
+drawing output n = mempty
+  <> addProperty (SvgEv.onClick $ \_ -> output (pred n)) (scale 0.8 $ redCircle $ negate n)
+  <> blueRect
+  <> addProperty (SvgEv.onClick $ \_ -> output (pred n)) (redCircle n)
+  <> xyAxis
+  <> smokeBackground
+  where
+    blueRect = fillColorA (Colors.blue `withOpacity` 0.5) $ scale 50 $ scaleX 1.2 $ square
+    redCircle n = fillColor Colors.red $
+      translateY (5 * fromIntegral n) $ translateX (10 * fromIntegral n) $
+      scale (50 + 5 * fromIntegral n) $
+      circle
 
-cmap = contramapSink
 
 -- MAIN
 
