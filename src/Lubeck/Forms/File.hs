384fd1c60ae3f4ce1be843bd0625ac49d3ea3e01
Eugene <eugene.naumenko@gmail.com>
2016-02-03 16:54:31 +0200

    Select files widget progress

--- src/Lubeck/Forms/File.hs
+++ src/Lubeck/Forms/File.hs
@@ -13,6 +13,9 @@ import qualified Data.Map
 import Data.JSString (JSString, pack, unpack)
 
 import JavaScript.Web.XMLHttpRequest (FormDataVal(..))
+import qualified JavaScript.Web.File as WF
+import           JavaScript.Web.File
+import           JavaScript.Cast
 import GHCJS.Types
 
 import qualified Web.VirtualDom as VD
@@ -20,12 +23,22 @@ import qualified Web.VirtualDom.Html as E
 import qualified Web.VirtualDom.Html.Attributes as A
 import qualified Web.VirtualDom.Html.Events as Ev
 
+
 foreign import javascript unsafe "console.log($1)"
   elog :: Ev.Event -> IO ()
 
+foreign import javascript unsafe "console.log($1)"
+  vlog :: JSVal -> IO ()
+
 -- this belongs here https://github.com/BeautifulDestinations/virtual-dom/blob/master/src/Web/VirtualDom/Html/Events.hs
--- foreign import javascript unsafe "$1.target.files"
-  -- files :: Ev.Event -> [(JSString, FormDataVal)] <- ????
+foreign import javascript unsafe "Array.prototype.slice.call($1.target.files)"
+  files :: Ev.Event -> JSVal
+
+-- jsValToVal :: JSVal -> [File]
+-- jsValToVal = unsafeCast
+--
+-- filesToFormVal :: [File] -> [FormDataVal]
+-- filesToFormVal = fmap $ \f -> FileVal f (Just $ WF.name f)
 
 filesSelectWidget :: Widget' [(JSString, FormDataVal)]
 filesSelectWidget sink _ =
@@ -33,8 +46,17 @@ filesSelectWidget sink _ =
     [ A.class_ "form-control"
     , A.type_ "file"
     , Ev.change $ \e -> do
+          let fn = "images[]"
           elog e
-          -- sink Nothing
+
+          let f = files e
+          vlog f
+
+          -- let jsf = jsValToVal f
+          -- let formDataFiles = filesToFormVal jsf
+          -- let formFiles = zip (repeat fn) formDataFiles
+          --
+          -- sink formFiles
           return ()
     ] []
 
