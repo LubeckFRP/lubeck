8c90f3c6dfc08e892f22c2724ce417dfb5bc285d
Hans <hans@hanshoglund.se>
2016-02-04 00:34:28 +0000

    Factor out current plotting code to Plots.SimpleNormalized
    TODO come up with a better module hierarchy

--- src/Example.hs
+++ src/Example.hs
@@ -25,7 +25,7 @@ import Control.Concurrent(forkIO, threadDelay)
 import Control.Monad(forever)
 
 import Lubeck.FRP
-import Lubeck.Plots.SimpleNormalized (simpleLinePlot)
+import Lubeck.Plots.SimpleNormalized
 import Lubeck.App (Html, runAppReactive)
 import Lubeck.Forms
   -- (Widget, Widget', component, bothWidget)
