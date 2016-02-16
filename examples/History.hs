bfe07f11f0214f8bfc0951d0b4a57cbf44f5dd89
Hans Hoglund <hans@hanshoglund.se>
2016-02-16 12:15:08 -0500

    History example

---
+++ examples/History.hs
@@ -0,0 +1,29 @@
+
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings, QuasiQuotes, TemplateHaskell, OverloadedStrings, TupleSections #-}
+
+module Main where
+
+import Prelude hiding (div)
+import qualified Prelude
+
+import GHCJS.Types(JSString, jsval)
+-- import Web.VirtualDom.Html (p, h1, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label, ul, li)
+-- import Web.VirtualDom.Html.Events (click, change, keyup, submit, stopPropagation, preventDefault, value)
+-- import Web.VirtualDom.Html.Attributes (src, width, class_, href, target, width, src)
+import qualified Web.VirtualDom.Html as E
+import qualified Web.VirtualDom.Html.Attributes as A
+import qualified Web.VirtualDom.Html.Events as Ev
+
+import Lubeck.FRP
+import Lubeck.App (Html, runAppStatic)
+import JavaScript.Cast (cast) -- JSVal -> Maybe a
+import Lubeck.Web.History
+import Lubeck.FRP.History
+
+page :: Signal Html
+page = mempty
+
+-- MAIN
+
+main :: IO ()
+main = runAppReactive page
