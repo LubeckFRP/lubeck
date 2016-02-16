7b67b035a4a3d59ca8e109c9533e8e4d94f173a3
Hans Hoglund <hans@hanshoglund.se>
2016-02-16 09:52:19 -0500

    Implement Fix Lubeck.Web.History imlementation

--- src/Lubeck/Web/History.hs
+++ src/Lubeck/Web/History.hs
@@ -16,6 +16,8 @@ import GHCJS.Types (JSVal)
 import GHCJS.Foreign.QQ (js_, jsu_)
 import GHCJS.Types(JSString, jsval)
 
+import GHCJS.Foreign.Callback (Callback, syncCallback1, OnBlocked(ThrowWouldBlock))
+
 go :: Int -> IO ()
 go n = [jsu_| window.history.go(`n) |]
 
@@ -23,6 +25,9 @@ pushState :: JSVal -> JSString -> JSString -> IO ()
 pushState state title url = [jsu_| window.history.pushState(`state, `title, `url) |]
 
 onPopState :: (JSVal -> IO ()) -> IO ()
-onPopState = error "TODO"
--- onPopState cb = [jsu_| window.onpopstate = function(e) { `cb(e.state); console.log("State!") } |]
--- TODO figure out how to quote the callback, or use standard FFI
+onPopState f = do
+  cb <- syncCallback1 ThrowWouldBlock f
+  js_onPopState cb
+
+foreign import javascript unsafe "window.onpopstate($1)"
+  js_onPopState :: Callback (JSVal -> IO ()) -> IO ()
