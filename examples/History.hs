e4d3f8dc56894108a3cd0fdddf89898988ae8cd6
Hans Hoglund <hans@hanshoglund.se>
2016-02-16 12:21:33 -0500

    History example

--- examples/History.hs
+++ examples/History.hs
@@ -18,12 +18,17 @@ import Lubeck.FRP
 import Lubeck.App (Html, runAppStatic)
 import JavaScript.Cast (cast) -- JSVal -> Maybe a
 import Lubeck.Web.History
+import Lubeck.Forms
+import Lubeck.Forms.Basic (rangeWidget)
 import Lubeck.FRP.History
 
-page :: Signal Html
-page = mempty
+page :: IO (Signal Html)
+page = do
+  (inputView, intsE) <- componentEvent 0 (rangeWidget 0 100 1) mempty
+  let outputView   = componentListen integerWidget (stepperS 0 intsE)
+  return $ mconcat [inputView, outputView]
 
 -- MAIN
 
 main :: IO ()
-main = runAppReactive page
+main = page >>= runAppReactive
