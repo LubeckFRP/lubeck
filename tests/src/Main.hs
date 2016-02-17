5fcb490a8f88862e7828fb47a1c118e467c00fcb
Eugene <eugene.naumenko@gmail.com>
2016-02-17 11:15:53 +0200

    testLoginFail >> testLoginOk

--- tests/src/Main.hs
+++ tests/src/Main.hs
@@ -14,7 +14,7 @@ firefoxConfig :: WDConfig
 firefoxConfig = defaultConfig
 chromeConfig = useBrowser chrome defaultConfig
 
-testLogin = do
+testLoginFail = do
   openPage "http://localhost:8090/adplatform/"
 
   waitUntil 15 $ expect . (== "http://localhost:8090/adplatform/") =<< getCurrentURL
@@ -36,9 +36,32 @@ testLogin = do
     errorMsg  <- findElem ( ByCSS "body > div > div > div.notifPanel > div > div > div > span" )
     expect . ("Sorry" `T.isInfixOf`)  =<< (getText errorMsg)
 
+
+testLoginOk = do
+  openPage "http://localhost:8090/adplatform/"
+
+  waitUntil 15 $ expect . (== "http://localhost:8090/adplatform/") =<< getCurrentURL
+
+  userInput  <- findElem ( ById "username-input" )
+  passInput  <- findElem ( ById "password-input" )
+  loginInput <- findElem ( ById "login-submit" )
+
+  -- clearInput userInput
+  -- sendKeys "Hello," userInput
+
+  -- clearInput passInput
+  -- sendKeys "World!" passInput
+  -- sendKeys "\t" passInput -- generate `change` event to trigger form validation
+
+  click loginInput
+
+  waitUntil 15 $ do
+    errorMsg  <- findElem ( ByCSS "body > div > div.row > div > nav > div > div > div.navbar-collapse > ul.nav.navbar-nav.navbar-right > li > a" )
+    expect . ("Logout" `T.isInfixOf`)  =<< (getText errorMsg)
+
   -- closeSession
 
-testCase c = void . runSession c . finallyClose $ testLogin
+testCase c = void . runSession c . finallyClose $ (testLoginFail >> testLoginOk)
 
 
 main = do
