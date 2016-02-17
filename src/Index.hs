7dc30acb7e6858a5fa4dca57b931a86798f0084f
Hans Hoglund <hans@hanshoglund.se>
2016-02-17 09:10:57 -0500

    Cleanup index

--- src/Index.hs
+++ src/Index.hs
@@ -7,7 +7,7 @@ import Prelude hiding (div)
 import qualified Prelude
 
 import GHCJS.Types(JSString, jsval)
-import Web.VirtualDom.Html (p, h1, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label, ul, li)
+import Web.VirtualDom.Html (p, h1, h2, div, text, form, button, img, hr, a, table, tbody, th, tr, td, input, label, ul, li)
 import Web.VirtualDom.Html.Events (click, change, keyup, submit, stopPropagation, preventDefault, value)
 import Web.VirtualDom.Html.Attributes (src, width, class_, href, target, width, src)
 import qualified Web.VirtualDom.Html as E
@@ -18,6 +18,7 @@ import Lubeck.App (Html, runAppStatic)
 
 page :: Html
 page = div [] [h1 [] [text "Index"]
+  , h2 [] [text "Applications"]
   , ul []
     [ li [] [a [href "adplatform/"]      [text "Ad platform"]]
     , li [] [a [href "interactions/"]    [text "Interaction browser"]]
@@ -26,6 +27,33 @@ page = div [] [h1 [] [text "Index"]
     , li [] [a [href "example-api-req/"] [text "Example: BD Api"]]
     , li [] [a [href "example-history/"] [text "Example: History"]]
     , li [] [a [href "example/"]         [text "Example: Reactive page"]]
+    , li [] [a [href "example-plots/"]   [text "Example: DV 1"]]
+    , li [] [a [href "doc/"]             [text "Haddock docs"]]
+    , li [] [a [href "https://www.stackage.org/nightly-2015-12-14"] [text "Stackage docs"]]
+    ]
+  , h2 [] [text "Examples"]
+  , ul []
+    [ li [] [a [href "adplatform/"]      [text "Ad platform"]]
+    , li [] [a [href "interactions/"]    [text "Interaction browser"]]
+    , li [] [a [href "example-static/"]  [text "Example: Static page"]]
+    , li [] [a [href "example-dynamic/"] [text "Example: Counters"]]
+    , li [] [a [href "example-api-req/"] [text "Example: BD Api"]]
+    , li [] [a [href "example-history/"] [text "Example: History"]]
+    , li [] [a [href "example/"]         [text "Example: Reactive page"]]
+    , li [] [a [href "example-plots/"]   [text "Example: DV 1"]]
+    , li [] [a [href "doc/"]             [text "Haddock docs"]]
+    , li [] [a [href "https://www.stackage.org/nightly-2015-12-14"] [text "Stackage docs"]]
+    ]
+  , h2 [] [text "Documentation"]
+  , ul []
+    [ li [] [a [href "adplatform/"]      [text "Ad platform"]]
+    , li [] [a [href "interactions/"]    [text "Interaction browser"]]
+    , li [] [a [href "example-static/"]  [text "Example: Static page"]]
+    , li [] [a [href "example-dynamic/"] [text "Example: Counters"]]
+    , li [] [a [href "example-api-req/"] [text "Example: BD Api"]]
+    , li [] [a [href "example-history/"] [text "Example: History"]]
+    , li [] [a [href "example/"]         [text "Example: Reactive page"]]
+    , li [] [a [href "example-plots/"]   [text "Example: DV 1"]]
     , li [] [a [href "doc/"]             [text "Haddock docs"]]
     , li [] [a [href "https://www.stackage.org/nightly-2015-12-14"] [text "Stackage docs"]]
     ]
