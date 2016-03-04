0ab24d16c89b35ac5473d4d87443463f54df66db
Hans Hoglund <hans@hanshoglund.se>
2016-03-04 16:29:58 -0500

    DV tests

--- examples/DV5.hs
+++ examples/DV5.hs
@@ -1,5 +1,5 @@
 
-{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings, OverloadedStrings, TupleSections #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings, OverloadedStrings, TupleSections, ScopedTypeVariables #-}
 
 module Main where
 
@@ -15,6 +15,7 @@ import qualified Data.Ord
 import System.Random (mkStdGen, randoms, split)
 import Control.Lens (view)
 import Control.Lens.Operators
+import Control.Applicative
 
 import Lubeck.FRP
 import Lubeck.App (Html, runAppReactive)
@@ -34,8 +35,14 @@ import qualified Data.Colour.Names as Colors
 Basic GUI examples:
 -}
 
+-- | FRP routing monad. Should be MonadIO but not IO. Could be more specific, i.e. separating polling/sending etc a la reflex.
+type FRP = IO
 
-facet :: a -> WidgetT r a (a -> a) -> IO (Signal r, Signal a, Sink (a -> a))
+-- A facet is much like a widget or component.
+type Facet   r a b = (Signal r, Signal b, Sink a)
+type FacetO  r b   = (Signal r, Signal b)
+
+facet :: a -> WidgetT r a (a -> a) -> FRP (Facet r (a -> a) a)
 facet initialState widget = do
   (internalSink, internalEvents) <- newEvent
   aS              <- accumS initialState internalEvents
@@ -44,31 +51,39 @@ facet initialState widget = do
 
 facet2  :: (b -> a -> a) -> (c -> a -> a)
         -> a -> WidgetT r a c
-        -> IO (Signal r, Signal a, Sink b)
+        -> FRP (Facet r b a)
 facet2 f g z w = do
   (v,o,i) <- facet z (rmapWidget g w)
   return (v,o,contramapSink f i)
 
+facetOutputOnly :: Monoid a => WidgetT r a c -> FRP (FacetO r a)
+facetOutputOnly w = dropInput <$> facet2 (flip const) (flip const) mempty w
+  where
+    dropInput (v,o,i) = (v,o)
+
 
 -- Drawing-based GUI
 
-
-
 clickableDW :: WidgetT' Drawing ()
-clickableDW outp () = mconcat [sq,ci]
+clickableDW outp () = mconcat [ci,sq]
   where
     sq = fillColor Colors.grey   square
     ci = fillColor Colors.yellow circle
-clickableF :: ()
-clickableF = facet2 (flip const) (flip const) mempty clickableDW
-
+clickableF :: FRP (FacetO Drawing ())
+clickableF = facetOutputOnly clickableDW
 -- | A button that displays a boolan state and sends () when clicked.
 
 
 
-gui :: Signal Drawing
-gui = mempty
+gui :: FRP (Signal Drawing)
+gui = do
+  (v1,_) <- clickableF
+  (v2,_) <- clickableF
+  return $ liftA2 (|||) v1 v2
+  -- return mempty
 
-main = runAppReactive $ fmap (toSvg drawOpts . scale 200) gui
+main = do
+  (x :: Signal Html) <- fmap (fmap (toSvg drawOpts . scale 200)) $ gui
+  runAppReactive x
   where
     drawOpts = mempty { dimensions = P (V2 400 400)}
