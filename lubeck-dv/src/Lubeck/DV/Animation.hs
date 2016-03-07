3cc1d474bb67b62d59ecb1ee8fff947d4dbf4bf1
Hans Hoglund <hans@hanshoglund.se>
2016-03-07 09:45:54 -0500

    DV: Fix Animations/Animation confusion

--- lubeck-dv/src/Lubeck/DV/Animation.hs
+++ lubeck-dv/src/Lubeck/DV/Animation.hs
@@ -1,4 +1,12 @@
 
 module Lubeck.DV.Animation
-  ()
+  (
+    AnimatedT(..)
+  )
 where
+
+import Control.Monad.Identity
+import Control.Monad.Reader
+import Data.Time (DiffTime)
+
+type AnimatedT m a = ReaderT DiffTime m a
