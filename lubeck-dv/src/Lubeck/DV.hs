a5a6d456993d0e8e4581fbc5d894d36d9892f136
Hans Hoglund <hans@hanshoglund.se>
2016-04-17 13:41:59 -0400

    Break out Str type
    Close #89

--- lubeck-dv/src/Lubeck/DV.hs
+++ lubeck-dv/src/Lubeck/DV.hs
@@ -2,18 +2,14 @@
 {-# LANGUAGE CPP #-}
 
 module Lubeck.DV
-  ( module Lubeck.DV.Animation
-  , module Lubeck.DV.Drawing
-  , module Lubeck.DV.Interactive
+  ( module Lubeck.DV.Drawing
   , module Lubeck.DV.New
   , module Lubeck.DV.SimpleNormalized
   , module Lubeck.DV.Styling
   )
 where
 
-import Lubeck.DV.Animation
 import Lubeck.DV.Drawing
-import Lubeck.DV.Interactive
 import Lubeck.DV.New
 import Lubeck.DV.SimpleNormalized
 import Lubeck.DV.Styling
