f384e5557aaaa8fdb879d9e93375cc7af249bfa2
Hans Hoglund <hans@hanshoglund.se>
2016-04-29 15:55:13 -0400

    DV test app

--- lubeck-dv-tests/app/Main.hs
+++ lubeck-dv-tests/app/Main.hs
@@ -15,24 +15,29 @@ import Lubeck.Forms.Button(multiButtonWidget)
 import Lubeck.App(runAppReactive)
 import Web.VirtualDom.Html(text)
 
-plusMinus label = do
-  (view, add :: Events Double) <- componentEvent 0 (multiButtonWidget [("-", -1), ("+", 1)]) mempty
-  zoom <- accumS 0 (fmap (+) add)
+plusMinus label init = do
+  (view, alter :: Events (Double -> Double)) <- componentEvent id (multiButtonWidget [("-", (/ 1.1)), ("+", (* 1.1))]) mempty
+  zoom <- accumS init alter
   return (mconcat [pure $ text label, view], zoom)
 
 main :: IO ()
 main = do
-  (view1, zoomX) <- plusMinus "Zoom X"
-  (view2, zoomY) <- plusMinus "Zoom Y"
+  (view1, zoomX) <- plusMinus "Zoom X" 1
+  (view2, zoomY) <- plusMinus "Zoom Y" 1
   let zoomXY = liftA2 V2 zoomX zoomY
 
   -- Debug
   subscribeEvent (updates zoomXY) print
 
-  let (plotSD :: Styled Drawing) = drawPlot $
-          plot (zip [1..10::Double] [31,35,78,23,9,92,53,71,53,42::Double])
-            [x <~ _1, y <~ _2]
-            (mconcat [line, fill])
-  let plotVD = toSvg mempty (getStyled plotSD (zoom .~ (V2 1 1) $ mempty))
+  let (plotSD :: Styled Drawing) = drawPlot $ mconcat
+          [ plot (zip [1..10::Double] [31,35,78,23,9,92,53,71,53,42::Double])
+              [x <~ _1, y <~ _2]
+              (mconcat [line, fill])
+          , plotLabel "(4,50)" [(4::Double, 50::Double)]
+              [x <~ _1, y <~ _2]
+          , plotLabel "(7,65)" [(7::Double, 65::Double)]
+              [x <~ _1, y <~ _2]
+          ]
 
-  runAppReactive $ mconcat [view1, view2, pure plotVD]
+  let plotVD = fmap (\currentZoom -> toSvg mempty (getStyled plotSD (zoom .~ currentZoom $ mempty))) zoomXY
+  runAppReactive $ mconcat [view1, view2, plotVD]
