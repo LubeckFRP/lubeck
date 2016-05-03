6e1999f170d1d6041ad823ad9ef9d82435843c40
Hans Hoglund <hans@hanshoglund.se>
2016-05-03 15:25:07 +0100

    Test zoom/drag handler embed

--- lubeck-dv-tests/app/Main.hs
+++ lubeck-dv-tests/app/Main.hs
@@ -5,20 +5,63 @@ module Main where
 
 import BasePrelude
 import Control.Lens(to, _1, _2, (.~))
+import Linear.Affine ((.+^))
 -- import Linear.V2 (V2(..))
 
 import Lubeck.DV
 import Lubeck.FRP
-import Lubeck.Drawing(Drawing, toSvg, V2(..))
+import Lubeck.Str
+import Lubeck.Drawing hiding (text)
+-- import Lubeck.Drawing(Drawing, toSvg, V2(..))
 import Lubeck.Forms(componentEvent)
 import Lubeck.Forms.Button(multiButtonWidget)
 import Lubeck.App(runAppReactive)
-import Web.VirtualDom.Html(text)
+import Web.VirtualDom.Html(Html, h1, text)
 
+-- TODO separate/consolidate Html/Svg events
+-- Currently they can be used more or less interchangebly
+import Web.VirtualDom.Html.Events(Event, mousemove)
+
+import Data.Colour.Names as Colors
+
+----------
+
+-- TODO handle onmousedown, onmousemove, onmouseout, onmouseup
+-- Ref: http://www.petercollingridge.co.uk/interactive-svg-components/draggable-svg-element
+
+-- From mouse events we can read various properties (clientX, screenX, movementX)
+-- https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/movementX
+-- Easiest would be to just use movementX!
+
+foreign import javascript unsafe "$1.movementX"
+  movementX :: Event -> Double
+foreign import javascript unsafe "$1.movementY"
+  movementY :: Event -> Double
+
+-- TODO move
+draggable :: Drawing -> FRP (Signal Drawing, Signal (P2 Double))
+draggable d =  do
+  (move, moved :: Events (V2 Double)) <- newEvent
+  (pos :: Signal (P2 Double)) <- accumS (P $ V2 0 0) (fmap (^+.) moved)
+
+  let dWithHandlers = (addProperty (mousemove (handleMouseMove move)) d)
+
+  -- TODO handle down/up/out
+
+  return $ (fmap (\(P v) -> translate v dWithHandlers) pos, pos)
+  where
+    (^+.) = flip (.+^)
+    handleMouseMove dest ev = do
+      dest $ V2 (movementX ev) (-movementY ev)
+      return ()
+
+----------
+
+plusMinus :: Str -> Double -> IO (Signal Html, Signal Double)
 plusMinus label init = do
   (view, alter :: Events (Double -> Double)) <- componentEvent id (multiButtonWidget [("-", (/ 1.1)), ("+", (* 1.1))]) mempty
   zoom <- accumS init alter
-  return (mconcat [pure $ text label, view], zoom)
+  return (mconcat [pure $ text (toJSString label), view], zoom)
 
 main :: IO ()
 main = do
@@ -40,4 +83,13 @@ main = do
           ]
 
   let plotVD = fmap (\currentZoom -> toSvg mempty (getStyled plotSD (zoom .~ currentZoom $ mempty))) zoomXY
-  runAppReactive $ mconcat [view1, view2, plotVD]
+  -- runAppReactive $ mconcat [pure $ h1 [] [text "Zoom test"], view1, view2, plotVD]
+
+  let purpleCircle = Lubeck.Drawing.fillColor Colors.purple $ Lubeck.Drawing.scale 190 circle
+  let pinkCircle = Lubeck.Drawing.fillColor Colors.pink $ Lubeck.Drawing.scale 150 circle
+
+  dc1 <- fmap fst $ draggable pinkCircle
+  dc2 <- fmap fst $ draggable purpleCircle
+  let (sd :: Signal Drawing) = mconcat [fmap (translateX 120) dc1, dc2]
+
+  runAppReactive $ fmap (toSvg mempty) $ sd
