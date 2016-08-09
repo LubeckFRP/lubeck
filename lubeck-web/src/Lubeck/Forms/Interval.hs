5120a546dca88fe2dc9c371a5216a5fe76f5c9dd
EugeneN <eugene.naumenko@gmail.com>
2016-08-10 01:34:05 +0300

    dateIntervalWidgetNoLabel

--- lubeck-web/src/Lubeck/Forms/Interval.hs
+++ lubeck-web/src/Lubeck/Forms/Interval.hs
@@ -6,6 +6,7 @@ module Lubeck.Forms.Interval
   ( integerIntervalWidget
   , dateIntervalWidget
   , customIntervalWidget
+  , dateIntervalWidgetNoLabel
   , IntervalSpec(..)
   ) where
 
@@ -38,6 +39,23 @@ integerIntervalWidget = customIntervalWidget 0 hideableIntegerWidget
 dateIntervalWidget :: Day -> JSString -> IntervalSpec -> Widget' (Interval Day)
 dateIntervalWidget dayNow = customIntervalWidget dayNow hideableDateWidget
 
+-- | A widget for selecting a date range, with no label
+dateIntervalWidgetNoLabel :: Day -> IntervalSpec -> Widget' (Interval Day)
+dateIntervalWidgetNoLabel dayNow = customIntervalWidgetNoLabel dayNow hideableDateWidget
+
+customIntervalWidgetNoLabel
+  :: forall a. Ord a
+  => a                      -- ^ Default value, used i.e. when switching from \"Any\" to an interval with endpoints.
+  -> (Bool -> Widget' a)    -- ^ Underlying widget type. Argument is @False@ whenever the widget disabled,
+                            --   i.e. because the endpoint is not in use.
+  -> IntervalSpec           -- ^ Choose whether IntervalRange is static or malleable
+  -> Widget' (Interval a)
+customIntervalWidgetNoLabel z numW spec =
+  case spec of
+      Bounded -> dimapWidget (snd . fromInterval z) toIntervalBetween $ numsW numW Between
+      Varying -> dimapWidget (fromInterval z) toInterval (spanWidget2 numW)
+
+
 -- |
 -- Create a widget for intervals of arbitrary ordered type, based on an underlying widget.
 --
@@ -52,59 +70,57 @@ customIntervalWidget
   -> JSString               -- ^ Title
   -> IntervalSpec           -- ^ Choose whether IntervalRange is static or malleable
   -> Widget' (Interval a)
-customIntervalWidget z numW title spec = 
-    mapHtmlWidget (\x -> E.div [A.class_ "form-group"]
-      [ E.label [A.class_ "control-label col-xs-2"] [E.text title]
-      , E.div [A.class_ "col-xs-10 form-inline"] [x]
-      ]) $ case spec of 
-        Bounded -> dimapWidget (snd . fromInterval) toIntervalBetween $ numsW Between 
-        Varying -> dimapWidget fromInterval toInterval spanWidget2 
-  where
-    fromInterval :: Interval a -> (IntervalRange, (a,a))
-    fromInterval i = case (lowerBound i, upperBound i) of
-      (NegInf,    PosInf)     -> (Any,          (z,z))
-      (NegInf,    Finite b)   -> (LessThan,     (b,b))
-      (Finite a,  PosInf)     -> (GreaterThan,  (a,a))
-      (Finite a,  Finite b)   -> (Between,      (a, b))
-      _                       -> (Any,          (z,z)) -- empty
+customIntervalWidget z numW title spec =
+  mapHtmlWidget (\x -> E.div [A.class_ "form-group"]
+    [ E.label [A.class_ "control-label col-xs-2"] [E.text title]
+    , E.div [A.class_ "col-xs-10 form-inline"] [x]
+    ]) $ case spec of
+      Bounded -> dimapWidget (snd . fromInterval z) toIntervalBetween $ numsW numW Between
+      Varying -> dimapWidget (fromInterval z) toInterval (spanWidget2 numW)
 
-    toInterval :: (IntervalRange, (a,a)) -> Interval a
-    toInterval x = case x of
-      (Any,         (_,_)) -> interval (NegInf,True) (PosInf,True)
-      (GreaterThan, (x,_)) -> interval (Finite x,True) (PosInf,True)
-      (LessThan,    (_,y)) -> interval (NegInf,True) (Finite y,True)
-      -- If x > y, we don't want to generate empty, so increase upper bound to match lower bound
-      (Between,     (x,y)) ->
-        if x > y
-            then interval (Finite x,True) (Finite x,True)
-            else interval (Finite x,True) (Finite y,True)
+fromInterval :: a -> Interval a -> (IntervalRange, (a,a))
+fromInterval z i = case (lowerBound i, upperBound i) of
+  (NegInf,    PosInf)     -> (Any,          (z,z))
+  (NegInf,    Finite b)   -> (LessThan,     (b,b))
+  (Finite a,  PosInf)     -> (GreaterThan,  (a,a))
+  (Finite a,  Finite b)   -> (Between,      (a, b))
+  _                       -> (Any,          (z,z)) -- empty
 
-    toIntervalBetween :: (a,a) -> Interval a
-    toIntervalBetween (x,y) = toInterval (Between, (x,y)) 
+toInterval :: Ord a => (IntervalRange, (a,a)) -> Interval a
+toInterval x = case x of
+  (Any,         (_,_)) -> interval (NegInf,True) (PosInf,True)
+  (GreaterThan, (x,_)) -> interval (Finite x,True) (PosInf,True)
+  (LessThan,    (_,y)) -> interval (NegInf,True) (Finite y,True)
+  -- If x > y, we don't want to generate empty, so increase upper bound to match lower bound
+  (Between,     (x,y)) ->
+    if x > y
+        then interval (Finite x,True) (Finite x,True)
+        else interval (Finite x,True) (Finite y,True)
 
-    spanWidget2 :: Widget' (IntervalRange, (a,a))
-    spanWidget2 s x = composeWidget spanTypeW (numsW $ fst x) s x
+toIntervalBetween :: Ord a => (a,a) -> Interval a
+toIntervalBetween (x,y) = toInterval (Between, (x,y))
 
-    spanTypeW :: Widget' IntervalRange
-    spanTypeW = selectWidget
-      [ (Any,         "Any")
-      , (GreaterThan, "Greater than")
-      , (LessThan,    "Less than")
-      , (Between,     "Between")
-      ]
+spanWidget2 :: (Bool -> Widget' a) -> Widget' (IntervalRange, (a,a))
+spanWidget2 numW s x = composeWidget spanTypeW (numsW numW $ fst x) s x
 
-    numsW :: IntervalRange -> Widget' (a, a)
-    numsW infFin = composeWidget (numW (fst $ visible infFin)) (numW (snd $ visible infFin))
+spanTypeW :: Widget' IntervalRange
+spanTypeW = selectWidget
+  [ (Any,         "Any")
+  , (GreaterThan, "Greater than")
+  , (LessThan,    "Less than")
+  , (Between,     "Between")
+  ]
 
-    visible :: IntervalRange -> (Bool,Bool)
-    visible x = case x of
-      Any         -> (False, False)
-      GreaterThan -> (True, False)
-      LessThan    -> (False, True)
-      Between     -> (True, True)
+numsW :: (Bool -> Widget' a) -> IntervalRange -> Widget' (a, a)
+numsW numW infFin = composeWidget (numW (fst $ visible infFin)) (numW (snd $ visible infFin))
 
-    -- TODO is the (Monoid Html) instance what we need?
-    composeWidget :: Widget' b -> Widget' c -> Widget' (b,c) 
-    composeWidget a b = bothWidget mappend (subWidget Control.Lens._1 a) (subWidget Control.Lens._2 b)
+visible :: IntervalRange -> (Bool,Bool)
+visible x = case x of
+  Any         -> (False, False)
+  GreaterThan -> (True, False)
+  LessThan    -> (False, True)
+  Between     -> (True, True)
 
-
+-- TODO is the (Monoid Html) instance what we need?
+composeWidget :: Widget' b -> Widget' c -> Widget' (b,c)
+composeWidget a b = bothWidget mappend (subWidget Control.Lens._1 a) (subWidget Control.Lens._2 b)
