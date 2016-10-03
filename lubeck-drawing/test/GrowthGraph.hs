1a4cf9a5a7ee4f38119c86312fa2ed1d55a962d2
Hans Hoglund <hans@hanshoglund.se>
2016-10-03 16:14:18 +0100

    Growth graph demo

--- lubeck-drawing/test/GrowthGraph.hs
+++ lubeck-drawing/test/GrowthGraph.hs
@@ -25,6 +25,8 @@ import Lubeck.Drawing.Internal.Backend.FastRenderer (adaptCoordinates, prerender
   , showRenderer
   , renderFastDrawing
   , runRenderingLoop
+  , Context
+  , MouseEventType(..)
 
   , getPointTag
   , TagResult(..)
@@ -78,53 +80,72 @@ import Linear.Epsilon
 
 
 
+-- TODO move
+showLocalOrigin x = showPoint (P (V2 0 0)) <> x
+
 
 -- growthGraph :: Behavior (Draft Fast)
+
+-- Using top-left local origin
 growthGraph :: Draft Fast
-growthGraph = mconcat
-  [ title
+growthGraph = translate (V2 50 (-50))
+  $ mconcat
+  [ mempty
+  , title
   , translate (V2 200 0) moreFollowersThisPeriod
   --
-  , followersLikeCommentFilter
-  -- , periodShortCut
-  -- , growthPlotUsingDV -- TODO connect to overlays
-  -- , maybeOverlay -- The whole thing, with count, likes/comm, image, larger point, alternative pointer
-  -- , bottomSelector
+  , translateY (-50) $ mconcat
+    [ mempty
+    , showLocalOrigin $ followersLikeCommentFilter
+    , translateX 200 periodShortCut
+    ]
+  , translateY (-100) $ mconcat
+    [ mempty
+    , growthPlotUsingDV -- TODO connect to overlays
+    -- , maybeOverlay -- The whole thing, with count, likes/comm, image, larger point, alternative pointer
+    ]
+  , translateY (-500) $ mconcat
+    [ mempty
+    , bottomSelector
+    ]
   ]
 
 title :: Draft Fast
-title = titleText <> translate (V2 80 0) explanation
+title = titleText <> translate (V2 120 0) explanation
   where
     titleText = fillColor Colors.black $ text "Followers over time"
     explanation = mconcat
       [ fillColor Colors.white $ text "?"
-      , fillColor Colors.grey $ scale 10 circle
+      , fillColor Colors.lightgrey $ scale 10 circle
       ]
     -- TODO pop-up
     popUpText = "The growth of your followers during a selected time range"
 
 moreFollowersThisPeriod :: Draft Fast
-moreFollowersThisPeriod = mconcat [a,b,c]
+moreFollowersThisPeriod = mconcat [a, translateX 50 b, translateX 150 c]
   where
-    a = fillColor Colors.black $ text "2.3M"
-    b = fillColor Colors.black $ text "+30.97%"
+    a = fillColor Colors.lightgreen $ text "2.3M"
+    b = fillColor Colors.white $ text "+30.97%"<> (fillColor Colors.lightgreen $ scaleXY (V2 40 30) square)
     c = fillColor Colors.black $ text "MORE FOLLOWERS THIS PERIOD"
 
 followersLikeCommentFilter :: Draft Fast
-followersLikeCommentFilter = catH [b "Followers", b "Likes", b "Comments"]
+followersLikeCommentFilter = catH [b "Followers", translateX 90 $ b "Likes", translateX (90*2) $ b "Comments"]
   where
     -- TODO auto-match box size with text
     -- TODO hover/interact
-    b t = text t <> fillColor Colors.grey (scaleXY (V2 110 40) square)
+    b t = mconcat
+      [ fillColor Colors.white $ text t
+      , fillColor Colors.lightgrey (scaleXY (V2 110 40) square)
+      ]
 
 periodShortCut :: Draft Fast
 periodShortCut = catH [b "1d", b "5d", b "1m", b "3m", b "6m", b "YTD", b "ALL"]
   where
     -- TODO extra sapce
-    b t = fillColor Colors.grey (text t)
+    b t = fillColor Colors.lightgrey (text t)
 
 growthPlotUsingDV :: Draft Fast
-growthPlotUsingDV = mempty
+growthPlotUsingDV = fillColorA (Colors.black `withOpacity` 0.1) $ scaleXY (V2 800 200) squareTL
 -- TODO
 
 countAtTime :: Draft Fast
@@ -135,7 +156,7 @@ countAtTime = l <> t
     -- TODO auto-match box size with text
     -- TODO move to current mouseX (relative plot)
     l = (strokeColor Colors.white $ strokeWidth 2 $ scaleY 100 horizontalLine)
-    t = fillColor Colors.white (text "1,000,000") <> fillColor Colors.lightblue (scaleXY (V2 110 40) square)
+    t = fillColor Colors.white (text "1,000,000") <> fillColor Colors.lightblue (scaleXY (V2 110 40) squareTL)
 
 maybeOverlay :: Draft Fast
 maybeOverlay = catV [image, spaceV 20, bottom]
@@ -143,15 +164,16 @@ maybeOverlay = catV [image, spaceV 20, bottom]
     -- TODO correct image
     image = (fillColor Colors.red $ scaleXY (V2 90 90) square)
     -- TODO move to correct position
-    bottom = catH [commentD, text "1,023", likesD, text "299,222"] <> (fillColor Colors.lightgrey $ scaleXY (V2 110 30) square)
+    bottom = catH [commentD, text "1,023", likesD, text "299,222"] <> (fillColor Colors.lightgrey $ scaleXY (V2 110 30) squareTL)
     -- TODO proper comment/like image
     commentD = fillColor Colors.blue $ scale 10 circle
     likesD = fillColor Colors.red $ scale 10 circle
 
 bottomSelector :: Draft Fast
-bottomSelector = mempty
+bottomSelector = fillColor Colors.lightblue $ scaleXY (V2 800 10) squareTL
 
 
+squareTL = translate (V2 0.5 (-0.5)) $ square
 spaceH _ = mempty -- TODO
 spaceV _ = mempty -- TODO
 catV = mconcat -- TODO proper
@@ -159,16 +181,24 @@ catH = mconcat -- TODO proper
 
 
 
+data State = State Context (IORef Double)
 
 main = do
   runRenderingLoop init handleInput render
   print "GG"
   where
-    opts = RenderingOptions (P (V2 800 800)) Center False -- FIXME
-    init canvE ctxt renderer = return ()
-    handleInput () renderer eventType event  = return ()
-    render () renderer = do
-      renderFastDrawing renderer (adaptCoordinates opts $ getDraft growthGraph)
+    opts = RenderingOptions (P (V2 800 800)) TopLeft False -- FIXME
+    init canvE ctxt renderer = do
+      r <- newIORef 0
+      return $ State ctxt r
+    handleInput (State _ r) renderer eventType event = do
+      {-when (eventType == MouseDown) $-}
+      -- writeIORef r (offsetX event)
+      return ()
+    render (State ctxt r) renderer = do
+      clearRect ctxt 0 0 800 800
+      n <- readIORef r
+      renderFastDrawing renderer (adaptCoordinates opts $ getDraft $ rotate (realToFrac (n/800*(2*pi/5))) growthGraph)
       performMajorGC
       return ()
 #endif
